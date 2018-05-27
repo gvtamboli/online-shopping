96dc29af3fa0c31e7648e869e117af404a1cf69f
gvtamboli <gauravkumarv.tamboli@gmail.com>
2018-05-27 22:59:34 +0530

    updated BasicController

--- onlineshopping/src/main/java/com/gkv/onlineshopping/controller/BasicController.java
+++ onlineshopping/src/main/java/com/gkv/onlineshopping/controller/BasicController.java
@@ -2,6 +2,7 @@ package com.gkv.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,6 +13,14 @@ public class BasicController {
 		System.out.println("entered");
 		ModelAndView modelAndView = new ModelAndView("page");
 		modelAndView.addObject("msg", "greeting message test");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="/welcome")
+	public ModelAndView paramMethod(@RequestParam(name="user", defaultValue="Guest") String user) {
+		System.out.println("entered");
+		ModelAndView modelAndView = new ModelAndView("page");
+		modelAndView.addObject("msg", "Hello:"+user+", Welcome to onlineshopping");
 		return modelAndView;
 	}
 
